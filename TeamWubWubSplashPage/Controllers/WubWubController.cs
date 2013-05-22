bf107e029d2c6f1ccc951050445d32b4ce4f5abb
Josh Waldron <jwaldron@thejwal.com>
2013-05-22 17:35:36 -0500

    initial commit

---
+++ TeamWubWubSplashPage/Controllers/WubWubController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamWubWubSplashPage.Controllers
+{
+    public class WubWubController : Controller
+    {
+        //
+        // GET: /WubWub/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TeamWubWubSplashPage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamWubWubSplashPage.MvcApplication" Language="C#" %>
